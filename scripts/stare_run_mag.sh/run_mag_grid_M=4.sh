6a40ce19c9d4c4b898d328b43f808c3b39811878
Aleks <275350@student.pwr.edu.pl>
2026-08-13 13:36:19 +0200

    update

--- scripts/stare_run_mag.sh/run_mag_grid_M=4.sh
+++ scripts/stare_run_mag.sh/run_mag_grid_M=4.sh
@@ -58,7 +58,7 @@ for FID in no yes; do
         --include-fermion-identity "$FID" \
         --J-prime "$J_PRIME" \
         --grid-omega 1 \
-        --grid-g 201 \
+        --grid-g 80 \
         --eig-first 1 \
         --eig-last 10 \
         --omega-min 0.5 \
